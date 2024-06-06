992740cf1df4f293c67cbd87738a6e2fecc4d8ae
jang95 <ruin950521@naver.com>
2024-06-07 02:38:09 +0900

    fix: React-query setting staleTime & gcTime
    - Put the default image in the person who doesn't have a picture.

--- frontend/src/components/CrewCard.tsx
+++ frontend/src/components/CrewCard.tsx
@@ -1,6 +1,8 @@
 import { ActorDetail, CrewDetail } from '../store/crew';
 
 const FACE_URL = 'https://media.themoviedb.org/t/p/w276_and_h350_face/';
+const BASE_IMAGE_URL =
+  'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg';
 
 interface CrewCardProps {
   crewList: ActorDetail[] | CrewDetail[];
@@ -8,14 +10,22 @@ interface CrewCardProps {
 
 const CrewCard = ({ crewList }: CrewCardProps) => {
   const mainCrew = crewList.slice(0, 6);
+
   return (
     <>
       {mainCrew.map((crew) => {
         return (
-          <div className='max-w-[200px] shadow-lg bg-white mt-4 rounded-md mb-4'>
+          <div
+            key={crew.credit_id}
+            className='max-w-[200px] shadow-lg bg-white mt-4 rounded-md mb-4'
+          >
             <img
               className='w-full rounded-t-md'
-              src={`${FACE_URL}${crew.profile_path}`}
+              src={
+                crew.profile_path !== null
+                  ? `${FACE_URL}${crew.profile_path}`
+                  : BASE_IMAGE_URL
+              }
               alt={crew.name}
             />
             <div className='px-6 py-4'>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,14 @@ import './index.css';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
