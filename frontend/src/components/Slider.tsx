dcfb2a0d9da692c5eeadc89b98bf4163bb42b451
jang95 <ruin950521@naver.com>
2024-06-14 18:03:10 +0900

    refactor: Slider.tsx - Refactored for readability. - UseMemo for optimization.

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { fetchMoviesList } from '../api/moviesApi';
 import { MdArrowForwardIos, MdArrowBackIos } from 'react-icons/md';
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 const IMAGE_SIZE = 200;
 const INDEX_COUNT = 5;
+const IMAGE_LAST_INDEX = 20;
 
 interface SliderProps {
   type: string;
@@ -16,56 +17,70 @@ interface SliderProps {
 const Slider = ({ type, title }: SliderProps) => {
   const navigate = useNavigate();
 
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [lastIndex, setLastIndex] = useState(INDEX_COUNT);
+  const [indexRange, setIndexRange] = useState({ start: 0, end: INDEX_COUNT });
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: [type],
     queryFn: () => fetchMoviesList(type),
   });
+
+  const content = useMemo(() => {
+    if (data) {
+      const viewList = data.results.slice(indexRange.start, indexRange.end);
+      return viewList.map((image) => (
+        <div
+          key={image.id}
+          className={`gap-4 hover:cursor-pointer`}
+          onClick={() => navigate(`/movie/${image.id}`)}
+        >
+          <img
+            src={`${BASE_IMAGE_URL}w${IMAGE_SIZE}${image.poster_path}`}
+            alt={`Image ${image.title}`}
+            loading='lazy'
+          />
+        </div>
+      ));
+    }
+    return (
+      <div>
+        영화 정보를 가져오는데 실패 했습니다. 잠시 후 다시 시도해주세요.
+      </div>
+    );
+  }, [data, indexRange, navigate]);
+
+  const handleNext = () => {
+    setIndexRange((prev) => {
+      const newStart =
+        prev.start + INDEX_COUNT >= IMAGE_LAST_INDEX
+          ? 0
+          : prev.start + INDEX_COUNT;
+      const newEnd = newStart + INDEX_COUNT;
+      return { start: newStart, end: newEnd };
+    });
+  };
+
+  const handlePrev = () => {
+    setIndexRange((prev) => {
+      const newStart =
+        prev.start - INDEX_COUNT < 0
+          ? IMAGE_LAST_INDEX - INDEX_COUNT
+          : prev.start - INDEX_COUNT;
+      const newEnd = newStart + INDEX_COUNT;
+      return { start: newStart, end: newEnd };
+    });
+  };
 
   if (isLoading) {
     return <div>각 테마에 맞는 영화 불러오는 중...</div>;
   }
 
-  let content;
-
-  if (data) {
-    const viewList = data.results.slice(slideIndex, lastIndex);
-
-    content = viewList.map((image) => (
-      <div
-        key={image.id}
-        className={`gap-4 hover:cursor-pointer`}
-        onClick={() => navigate(`/movie/${image.id}`)}
-      >
-        <img
-          src={`${BASE_IMAGE_URL}w${IMAGE_SIZE}${image.poster_path}`}
-          alt={`Image ${image.title}`}
-        />
+  if (isError) {
+    return (
+      <div>
+        영화 정보를 가져오는데 실패 했습니다. 잠시 후 다시 시도해주세요.
       </div>
-    ));
+    );
   }
-
-  const handleNext = () => {
-    if (slideIndex < 15) {
-      setSlideIndex((prev) => prev + INDEX_COUNT);
-      setLastIndex((prev) => prev + INDEX_COUNT);
-    } else if (slideIndex === 15) {
-      setSlideIndex(0);
-      setLastIndex(INDEX_COUNT);
-    }
-  };
-
-  const handlePrev = () => {
-    if (slideIndex > 0) {
-      setSlideIndex((prev) => prev - INDEX_COUNT);
-      setLastIndex((prev) => prev - INDEX_COUNT);
-    } else if (slideIndex === 0) {
-      setSlideIndex(15);
-      setLastIndex(20);
-    }
-  };
 
   return (
     <div className='container mx-auto my-8'>
