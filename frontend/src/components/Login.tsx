3072eab81b413ec1c4db0730cd32016b71a83aac
jang95 <ruin950521@naver.com>
2024-08-10 01:09:12 +0900

    refacter: 'LoginPage'에서 Login관련 부분 분리 - Add: 'Login' 컴포넌트

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react';
+import { sendLoginRequest } from '../api/authApi';
+
+const Login = () => {
+  const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLoginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (idRef.current && passwordRef.current) {
+      const formData = new FormData();
+      formData.append('id', idRef.current.value);
+      formData.append('password', passwordRef.current.value);
+
+      sendLoginRequest(formData);
+    }
+  };
+  return (
+    <>
+      <p className='font-bold text-3xl p-6'>Sign In</p>
+
+      <form onSubmit={handleLoginSubmit}>
+        <div className='flex flex-col gap-4'>
+          <input
+            className='w-full h-10 px-8 rounded-xl focus:outline-none bg-gray-200 border-2 focus:border-blue-400'
+            ref={idRef}
+            type='text'
+            placeholder='Email or userName'
+          />
+          <input
+            className='w-full h-10 px-8 rounded-xl focus:outline-none bg-gray-200 border-2 focus:border-blue-400'
+            ref={passwordRef}
+            type='password'
+            placeholder='Password'
+          />
+          <button className='w-full h-10 rounded-xl bg-blue-400' type='submit'>
+            Sign In
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,57 +1,27 @@
-import { useRef } from 'react';
-import { sendLoginRequest } from '../../api/authApi';
+import { useState } from 'react';
+import Login from '../../components/Login';
 
 const LoginPage = () => {
-  const idRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-
-  const handleLoginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (idRef.current && passwordRef.current) {
-      const formData = new FormData();
-      formData.append('id', idRef.current.value);
-      formData.append('password', passwordRef.current.value);
-
-      sendLoginRequest(formData);
-    }
-  };
+  const [isLogin, setIsLogin] = useState<boolean>(true);
 
   return (
     <div className='flex w-[90%] h-[80vh]  mx-auto my-2 justify-center items-center'>
       <div className='w-[550px] h-[70%] bg-slate-600 bg-[url("assets/LoginBg.webp")] opacity-30 hidden lg:inline'></div>
 
       <div className='flex flex-col w-[390px] h-[70%] p-6 bg-gray-100 justify-center items-center'>
-        <p className='font-bold text-3xl p-6'>Sign In</p>
-        <div>
-          <form onSubmit={handleLoginSubmit}>
-            <div className='flex flex-col gap-4'>
-              <input
-                className='w-full h-10 px-8 rounded-xl focus:outline-none bg-gray-200 border-2 focus:border-blue-400'
-                ref={idRef}
-                type='text'
-                placeholder='Email or userName'
-              />
-              <input
-                className='w-full h-10 px-8 rounded-xl focus:outline-none bg-gray-200 border-2 focus:border-blue-400'
-                ref={passwordRef}
-                type='password'
-                placeholder='Password'
-              />
-              <button
-                className='w-full h-10 rounded-xl bg-blue-400'
-                type='submit'
-              >
-                Sign In
-              </button>
-              <button
-                className='w-full h-8 rounded-xl bg-green-400'
-                type='submit'
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
+        {isLogin ? <Login /> : null}
+        <div className='w-full my-4 border-b-[1px] border-y-slate-800'></div>
+        <div className='w-[50%]'>
+          <button
+            className={
+              isLogin
+                ? 'bg-green-400 w-full h-8 rounded-xl'
+                : 'bg-blue-400 w-full h-8 rounded-xl'
+            }
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? 'Sign up' : 'Sign In'}
+          </button>
         </div>
       </div>
     </div>
