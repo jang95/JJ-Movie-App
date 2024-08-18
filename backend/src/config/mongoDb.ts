625439fac0616dd997555f0fd745154eacfee860
jang95 <ruin950521@naver.com>
2024-08-19 01:00:46 +0900

    fix: mongoose.connection error

--- backend/src/config/mongoDb.ts
+++ backend/src/config/mongoDb.ts
@@ -3,22 +3,29 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
+// mongoose.connection는 Mongoose와 MongoDB 서버 간의 연결 상태를 나타내는 객체
 export const connectMongoDB = async () => {
-  // MongoDB에 연결
-  await mongoose.connect(process.env.MONGO_URI!);
-
   // 연결 성공 시 메시지 출력
   mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to MongoDB');
+    console.log('Mongoose가 MongoDB에 연결 성공!');
   });
 
   // 연결 실패 시 에러 메시지 출력
   mongoose.connection.on('error', (err) => {
-    console.error('Mongoose connection error:', err);
+    console.error('Mongoose 연결에 error 발생!!:', err);
   });
 
-  // 연결 종료 시 메시지 출력, Todo... 재연결 시도?
+  // 연결 종료 시 메시지 출력
   mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected from MongoDB');
+    console.log('Mongoose의 MongoDB 연결 종료!');
   });
+
+  try {
+    // MongoDB에 연결
+    await mongoose.connect(process.env.MONGO_URI!);
+  } catch (error) {
+    console.error('MongoDB에 연결하지 못했습니다.', error);
+    // 3초 후 재연결 시도
+    setTimeout(connectMongoDB, 3000);
+  }
 };
