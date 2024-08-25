a1dbaf3d5110b53d084588a45b5c6631ca535af6
jang95 <ruin950521@naver.com>
2024-08-25 17:25:41 +0900

    chore: Add 'cookie-parser'

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/', authRoutes);
 
