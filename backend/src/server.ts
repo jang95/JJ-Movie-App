879f8ebc1ea714e44dbf99892e44381aeb4ea9d7
jang95 <ruin950521@naver.com>
2024-08-31 00:00:01 +0900

    feat: Add mkcert

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,24 @@
 import app from './app';
 import { connectMongoDB } from './config/mongoDb';
+import https from 'https';
+import path from 'path';
+import fs from 'fs';
+
+const certPath = path.resolve(__dirname, '../../certs', 'cert.pem');
+const keyPath = path.resolve(__dirname, '../../certs', 'key.pem');
+
+const privateKey = fs.readFileSync(keyPath, 'utf8');
+const certificate = fs.readFileSync(certPath, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 const startServer = async () => {
   try {
     await connectMongoDB();
-    app.listen(process.env.PORT, () => {
-      console.log(`${process.env.PORT}번 포트 실행 완료`);
+    // app.listen(process.env.PORT, () => {
+    //   console.log(`${process.env.PORT}번 포트 실행 완료`);
+    // });
+    https.createServer(credentials, app).listen(process.env.PORT, () => {
+      console.log(`${process.env.PORT}번 포트에서 HTTPS 서버 실행 완료`);
     });
   } catch (error) {
     console.error('서버 연결 실패', error);
