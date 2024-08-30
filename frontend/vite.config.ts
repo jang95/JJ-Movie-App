48bf4849262ee0c7626f7f6a56634a03d0fea19b
jang95 <ruin950521@naver.com>
2024-08-31 03:57:05 +0900

    feat: Apply HTTPS settings Add: 'mkcert' authentication key.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
+const keyPath = path.resolve(__dirname, '../certs/key.pem');
+const certPath = path.resolve(__dirname, '../certs/cert.pem');
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    },
+    port: 5173,
+  },
+});
