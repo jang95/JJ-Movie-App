4e37a58e0ade6abf74f5f2219e10fe55831d8fef
jang95 <ruin950521@naver.com>
2024-09-09 21:42:55 +0900

    fix: Modify and imrove 'PrivateRoute' component

--- frontend/src/routes/PrivateRoute.tsx
+++ frontend/src/routes/PrivateRoute.tsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { parseJwt } from '../api/api';
 import { attemptTokenRefresh } from '../api/authApi';
 import { useAuthStore } from '../store/authStore';
-import { useParams } from 'react-router-dom';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
   const { accessToken } = useAuthStore();
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-
-  const { id } = useParams();
+  const [loading, setLoading] = useState<boolean>(true);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const checkAuth = async () => {
       if (accessToken) {
+        // 토큰이 있으면 만료 시간을 확인
         const decodedToken = parseJwt(accessToken);
         const tokenExpirationTime = new Date(decodedToken.exp * 1000);
         const currentTime = new Date();
@@ -25,31 +25,43 @@ const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
         if (tokenExpirationTime < currentTime) {
           try {
             await attemptTokenRefresh();
-            setIsAuthenticated(true);
           } catch (error) {
-            setIsAuthenticated(false);
+            console.error('토큰 갱신 실패:', error);
+            const moveLoginPage = confirm(
+              '로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?'
+            );
+            if (moveLoginPage) {
+              navigate('/login', {
+                state: { ...location.state, from: location },
+              });
+            } else {
+              navigate(-1);
+            }
           }
+        }
+      } else {
+        const moveLoginPage = confirm(
+          '로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?'
+        );
+        if (moveLoginPage) {
+          navigate('/login', {
+            state: { ...location.state, from: location },
+          });
         } else {
-          setIsAuthenticated(true);
+          navigate(-1);
         }
       }
+      setLoading(false);
     };
 
     checkAuth();
-  }, [accessToken]);
+  }, [accessToken, location, navigate]);
 
-  if (isAuthenticated) {
-    return <>{children}</>;
-  } else {
-    const moveLoginPage = confirm(
-      '인증된 사용자가 아닙니다. 로그인 페이지로 이동하시겠습니까?'
-    );
-    if (moveLoginPage) {
-      return <Navigate to='/login' replace />;
-    } else {
-      return <Navigate to={`/movie/${id}`} replace />;
-    }
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
+  return <>{children}</>;
 };
 
 export default PrivateRoute;
