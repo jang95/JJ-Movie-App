af71c9d3d270a20953c0479ef090ec12c8bf3880
jang95 <ruin950521@naver.com>
2024-09-09 22:49:43 +0900

    feat: Add review schema fix: user schema

---
+++ backend/src/schemas/review.ts
@@ -0,0 +1,72 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { IUser } from './user';
+
+export interface IReviewDetail {
+  title: string;
+  content: string;
+  rating: number;
+  tags?: string[];
+}
+
+export interface IMovie {
+  id: string;
+  title: string;
+}
+
+export interface IReview extends Document {
+  review: IReviewDetail;
+  author: IUser;
+  movie: IMovie;
+}
+
+const reviewSchema = new Schema<IReview>(
+  {
+    review: {
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      tags: [
+        {
+          type: String,
+          trim: true,
+        },
+      ],
+    },
+    author: {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+      },
+      nickName: {
+        type: String,
+        required: true,
+      },
+    },
+    movie: {
+      id: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model<IReview>('Review', reviewSchema);
+
+export default Review;
