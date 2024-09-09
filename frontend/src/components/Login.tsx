ec71bbf064dd3a83c756e3322c97b06696757cf6
jang95 <ruin950521@naver.com>
2024-09-09 21:40:49 +0900

    refactor: 'Login' component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,46 +1,94 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useState } from 'react';
 import { sendLoginRequest } from '../api/userApi';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
+import InputField from './form/InputField';
+
+interface Login {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
   const navigate = useNavigate();
-  const idRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+  const location = useLocation();
+  const [loginData, setLoginData] = useState<Login>({
+    email: '',
+    password: '',
+  });
 
   const { setUser, setAccessToken } = useAuthStore();
+
+  const createFormData = (data: Login) => {
+    const formData = new FormData();
+    formData.append('email', data.email);
+    formData.append('password', data.password);
+
+    return formData;
+  };
+
+  const submitLoginration = async (loginData: Login) => {
+    const data = createFormData(loginData);
+    try {
+      const userData = await sendLoginRequest(data);
+      setUser(userData.user);
+      setAccessToken(userData.accessToken);
+    } catch (error) {
+      alert('로그인에 실패했습니다. 다시 시도해주세요');
+    }
+  };
+
+  const movePage = () => {
+    const from = location.state?.from?.pathname || '/';
+    const { id, title, release, overview, poster } = location.state;
+    navigate(from, {
+      state: {
+        id,
+        title,
+        release,
+        overview,
+        poster,
+      },
+    });
+  };
 
   const handleLoginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (idRef.current && passwordRef.current) {
-      const formData = new FormData();
-      formData.append('email', idRef.current.value);
-      formData.append('password', passwordRef.current.value);
-      const userData = await sendLoginRequest(formData);
-      console.log('userData', userData);
-      setUser(userData.user);
-      setAccessToken(userData.accessToken);
-      navigate('/');
-    }
+    await submitLoginration(loginData);
+    movePage();
   };
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setLoginData((prevLoginData) => ({
+        ...prevLoginData,
+        [name]: value,
+      }));
+    },
+    [setLoginData]
+  );
+
   return (
     <>
       <p className='font-bold text-3xl p-6'>로그인</p>
 
       <form onSubmit={handleLoginSubmit}>
         <div className='flex flex-col gap-4'>
-          <input
-            className='w-full h-10 px-8 rounded-xl focus:outline-none bg-gray-200 border-2 focus:border-blue-400'
-            ref={idRef}
+          <InputField
             type='text'
-            placeholder='Email or userName'
+            name='email'
+            value={loginData.email}
+            placeholder='Your Email'
+            onChange={(e) => handleInputChange(e)}
           />
-          <input
-            className='w-full h-10 px-8 rounded-xl focus:outline-none bg-gray-200 border-2 focus:border-blue-400'
-            ref={passwordRef}
+          <InputField
             type='password'
-            placeholder='Password'
+            name='password'
+            value={loginData.password}
+            placeholder='Your Password'
+            onChange={(e) => handleInputChange(e)}
           />
           <button className='w-full h-10 rounded-xl bg-blue-400' type='submit'>
             로그인 하기
