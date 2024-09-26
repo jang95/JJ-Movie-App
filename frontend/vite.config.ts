c187a9341857727f703b163bf6d0255b23c9d56c
jang95 <ruin950521@naver.com>
2024-09-26 20:10:09 +0900

    chore: Update references

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 
-const keyPath = path.resolve(__dirname, '../certs/key.pem');
-const certPath = path.resolve(__dirname, '../certs/cert.pem');
+const keyPath = path.resolve(__dirname, 'certs/key.pem');
+const certPath = path.resolve(__dirname, 'certs/cert.pem');
 
 export default defineConfig({
   plugins: [react()],
