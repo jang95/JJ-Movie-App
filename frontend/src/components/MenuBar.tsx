96a322694bf84104806185fcac2598f6500e198f
jang95 <ruin950521@naver.com>
2024-10-09 01:14:04 +0900

    feat: Add navigation to mypage from menuBar

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -1,6 +1,11 @@
 import { sendLogoutRequest } from '../api/userApi';
+import { useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../store/authStore';
 
 const MenuBar = () => {
+  const navigate = useNavigate();
+  const { user } = useAuthStore();
+
   const userLogout = async () => {
     try {
       await sendLogoutRequest();
@@ -8,10 +13,18 @@ const MenuBar = () => {
       console.log(error);
     }
   };
+
+  const moveToMypage = () => {
+    navigate(`/mypage/${user?.nickName}`);
+  };
+
   return (
     <div className='absolute w-20 sm:w-28 top-full/2 cursor-pointer bg-white rounded-md border-2'>
       <ul>
-        <li className='text-xs sm:text-sm p-2 text-center border-b-2 hover:bg-slate-400'>
+        <li
+          className='text-xs sm:text-sm p-2 text-center border-b-2 hover:bg-slate-400'
+          onClick={moveToMypage}
+        >
           마이페이지
         </li>
         <li
